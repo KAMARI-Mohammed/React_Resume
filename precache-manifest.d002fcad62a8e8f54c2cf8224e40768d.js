d290e01728876b332f8a2fca9f68aad52549bdfc
Medk <medk12boy@gmail.com>
2023-11-20 18:24:04 +0100

    Updates

---
+++ precache-manifest.d002fcad62a8e8f54c2cf8224e40768d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec6956b001077c1cfb0921c3313bf0a",
+    "url": "/REACT-CV/index.html"
+  },
+  {
+    "revision": "af3a4ff94a6254a3ef8d",
+    "url": "/REACT-CV/static/css/main.ea66cac7.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/REACT-CV/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/REACT-CV/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "af3a4ff94a6254a3ef8d",
+    "url": "/REACT-CV/static/js/main.0c586114.chunk.js"
+  },
+  {
+    "revision": "144b376d15f3ec75c1e5",
+    "url": "/REACT-CV/static/js/runtime~main.806a8693.js"
+  }
+]);
